534b9718429c2d96a7bba358d3f300bc8a5f71cf
Angela Adams <aadams3867@gmail.com>
2016-10-10 17:23:19 -0400

    Fixes port conflict in server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use('/', routes);
 
 // Listener
 // ===========================================================
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
 	console.log('App listening on PORT ' + PORT);
 });
